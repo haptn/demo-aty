0edd861f33530e670f0f61e1388846511b81b4cf
Pham Thi Ngoc Ha <gauchocun@gmail.com>
2023-01-07 23:11:17 +0700

    Init project & add code base

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+// Images
+export { default as CoverImg } from './imgs/cover.png'
+export { default as LogoBgImg } from './imgs/logoATY.png'
+export { default as LogoNoBgImg } from './imgs/logo-aty.png'
+export { default as ATYLogoImg } from './imgs/logo.jpg'
+export { default as NewLogoImg } from './imgs/logo_new.jpg'
+
+// Icons
